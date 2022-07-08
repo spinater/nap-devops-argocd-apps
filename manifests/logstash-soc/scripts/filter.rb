ef63a307eb93bb213a263b2fe51c01b465ff5a7f
seubpong.mon <pjame.fb@gmail.com>
2022-07-08 19:55:29 +0700

    Extract hotspot category

--- manifests/logstash-soc/scripts/filter.rb
+++ manifests/logstash-soc/scripts/filter.rb
@@ -1,7 +1,20 @@
+#Example : https://fabianlee.org/2017/04/24/elk-using-ruby-in-logstash-filters/
+#Labels {'user', 'src_ip', 'dst_ip'}
+
 def register(params)
     @category = params["category"]
 end
 
 def filter(event)
+    if @category == 'hotspot'
+        arr1 = event['message'].split(',')
+        data1 = arr1[2]
+
+        arr2 = data1.split(':')
+        data2 = arr2[1]
+
+        event['user'] = data2
+    end
+
     return [event]
 end
