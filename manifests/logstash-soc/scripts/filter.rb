01969c9031668cc48079096349eb7966fdf7f377
seubpong.mon <pjame.fb@gmail.com>
2022-07-08 21:39:59 +0700

    Extract web-proxy category

--- manifests/logstash-soc/scripts/filter.rb
+++ manifests/logstash-soc/scripts/filter.rb
@@ -7,15 +7,17 @@ end
 
 def filter(event)
     arr1 = event.get('message').split(',')
+    category = arr1[0]
     event.set('debug_field1', 'debug_field1')
-    
-    if @category == 'hotspot'
+    event.set('debug_field2', @category)
+
+    if category == 'hotspot'
         data1 = arr1[2]
         arr2 = data1.split(':')
         data2 = arr2[1]
         event.set('user', data2)
         event.set('debug_field1', 'aaaaaaa')
-    elsif @category == 'web-proxy'
+    elsif category == 'web-proxy'
         data1 = arr1[1]
         #   0         1              2        3                  4                            5
         #account 0-Napbiotec: 192.168.20.115 GET http://cu.bwc.brother.com/certset/ver  action=allow cache=MISS
