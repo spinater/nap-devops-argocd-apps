8a2d569b7b4f0c074b2ad8919dcab25af9aa27ed
seubpong.mon <pjame.fb@gmail.com>
2022-07-13 10:41:09 +0700

    Test MISP cache logic

--- manifests/logstash-soc/scripts/load-misp-cache.rb
+++ manifests/logstash-soc/scripts/load-misp-cache.rb
@@ -6,10 +6,15 @@ end
 
 def filter(event)
     value = event.get(@value_field)
-    misp_data = "This is cached data of [#{value}]"    
 
-    puts "### Getting MISP from field [#{@value_field}] value [#{value}]"
-    puts "### Loading MISP data to field [#{@cached_field}]\n"
+    if value == ''
+        misp_data = "Nothing to do because the field [#{@value_field}] is blank [#{value}]"
+    else
+        misp_data = "This is cached data of [#{value}]"
+
+        puts "### Getting MISP from field [#{@value_field}] value [#{value}]"
+        puts "### Loading MISP data to field [#{@cached_field}]\n"    
+    end
 
     event.set(@cached_field, misp_data)
     return [event]
--- manifests/logstash-soc/scripts/process-misp-fields.rb
+++ manifests/logstash-soc/scripts/process-misp-fields.rb
@@ -5,9 +5,14 @@ end
 def filter(event)    
     misp_dst_ip = event.get('misp_dst_ip')
     misp_domain = event.get('misp_domain')
+    dst_ip = event.get('dst_ip')
+    domain = event.get('domain')
 
-    puts "#### Processing MISP data field [misp_dst_ip] value [#{misp_dst_ip}]"
-    puts "#### Processing MISP data field [misp_domain] value [#{misp_domain}]\n"
+    if dst_ip != ''
+        puts "#### Processing MISP data field [misp_dst_ip] value [#{misp_dst_ip}]"
+    elsif domain != ''
+        puts "#### Processing MISP data field [misp_domain] value [#{misp_domain}]\n"
+    end
 
     #event.set('misp_category', '')
 
