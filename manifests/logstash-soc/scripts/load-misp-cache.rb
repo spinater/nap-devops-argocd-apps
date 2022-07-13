2a7c370a6fe8a5e0fb306ddc96f870255ed8a3e5
seubpong.mon <pjame.fb@gmail.com>
2022-07-13 09:43:38 +0700

    Test MISP cache logic

--- manifests/logstash-soc/scripts/load-misp-cache.rb
+++ manifests/logstash-soc/scripts/load-misp-cache.rb
@@ -1,8 +1,17 @@
 def register(params)
     @cached_field = params["cached_field"]
+    @value_field = params["value_field"]
+    $stdout.sync = true
 end
 
 def filter(event)
-    event.set(@cached_field, '{}')
+    misp_data = '{"field":"this is MISP data"}'
+
+    value = event.get(@value_field)
+
+    stdout.puts "Getting MISP from field [#{@value_field}] value [#{value}]"
+    stdout.puts "Load [#{misp_data}] to field [#{@cached_field}]"
+
+    event.set(@cached_field, misp_data)
     return [event]
 end
