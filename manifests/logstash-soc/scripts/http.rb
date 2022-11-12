ca25ddacf1b9487110cc87b9ca05ff09103f3140
seubpong.mon <pjame.fb@gmail.com>
2022-11-12 14:22:28 +0700

    Send data to Loki

--- manifests/logstash-soc/scripts/http.rb
+++ manifests/logstash-soc/scripts/http.rb
@@ -11,16 +11,24 @@ end
 def filter(event)
     header = event.get('headers')
     nas_src = header['nas_source']
+    storage_data = event.get('StorageData')
 
     puts("DEBUG - Event hooked [#{nas_src}]")
 
     if (nas_src == 'office-hq')
         msg = event.get('Message Content').delete!("\n")
 
-        event.set('category', 'synology')
-        event.set('type', 'notification')
-
+        event.set('category', 'synology-noti')
+        event.set('type', 'webhook')
         event.set('message', msg)
+    elsif (!storage_data.nil?)
+        event.set('category', 'gcs-ftp')
+        event.set('type', 'webhook')
+        event.set('message', storage_data.to_json)
+    else
+        event.set('category', 'unknown')
+        event.set('type', 'webhook')
+        event.set('message', storage_data.to_json)        
     end
 
     return [event]
